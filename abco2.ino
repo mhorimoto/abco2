ef0900bf30f1fd42d9d1c22ad61f3b0888cdf3b1
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2019-09-23 11:06:54 +0900

    途中

--- abco2.ino
+++ abco2.ino
@@ -26,12 +26,12 @@ const byte U_InitPin_Sense=LOW;
 ////////////////////////////////////
 //Node basic infomation
 ///////////////////////////////////
-const char U_name[] PROGMEM= "ABCO2A03";//MAX 20 chars
+const char U_name[] PROGMEM= "ABCO2A04";//MAX 20 chars
 const char U_vender[] PROGMEM= "HOLLY";//MAX 20 chars
 const char U_uecsid[] PROGMEM= "10100C009999";//12 chars fixed
 const char U_footnote[] PROGMEM= "";
 //const int U_footnoteLetterNumber = 48;//Abolished after Ver 0.6
-char U_nodename[20] = "ABCO2A03";//MAX 19chars
+char U_nodename[20] = "ABCO2A04";//MAX 19chars
 UECSOriginalAttribute U_orgAttribute;
 
 //////////////////////////////////
@@ -40,18 +40,19 @@ UECSOriginalAttribute U_orgAttribute;
 #define DECIMAL_DIGIT	1 //小数桁数
 
 const int U_HtmlLine = 24; //Total number of HTML table rows.
-const char TempUNIT[] PROGMEM= "℃";
 
-//●熱電対温度センサの値
-//UECSSHOWDATA
-const char T1TEMP1[] PROGMEM= "T1温度";
-const char T1TEMP2[] PROGMEM= "T2温度";
-const char T1TEMP3[] PROGMEM= "T3温度";
-const char T1TEMP4[] PROGMEM= "T4温度";
-const char T1TEMP5[] PROGMEM= "T5温度";
-const char T1TEMP6[] PROGMEM= "T6温度";
-const char T1TEMP7[] PROGMEM= "T7温度";
-const char T1TEMP8[] PROGMEM= "T8温度";
+// 各種センサの値
+// センサの値は設定しないので UECSSHOWDATAで表示する。
+// 熱電対温度センサの値
+const char T1TEMP0[] PROGMEM= "T1温度";
+const char T1TEMP1[] PROGMEM= "T2温度";
+const char T1TEMP2[] PROGMEM= "T3温度";
+const char T1TEMP3[] PROGMEM= "T4温度";
+const char T1TEMP4[] PROGMEM= "T5温度";
+const char T1TEMP5[] PROGMEM= "T6温度";
+const char T1TEMP6[] PROGMEM= "T7温度";
+const char T1TEMP7[] PROGMEM= "T8温度";
+const char T1NOTE0[] PROGMEM= "";
 const char T1NOTE1[] PROGMEM= "";
 const char T1NOTE2[] PROGMEM= "";
 const char T1NOTE3[] PROGMEM= "";
@@ -59,29 +60,75 @@ const char T1NOTE4[] PROGMEM= "";
 const char T1NOTE5[] PROGMEM= "";
 const char T1NOTE6[] PROGMEM= "";
 const char T1NOTE7[] PROGMEM= "";
-const char T1NOTE8[] PROGMEM= "";
+const char TempUNIT[] PROGMEM= "℃";
 
 //熱電対温度センサの値
 //小数桁数が1の場合、123が12.3と表示される
-signed long showValueTemp1;
-signed long showValueTemp2;
-signed long showValueTemp3;
-signed long showValueTemp4;
-signed long showValueTemp5;
-signed long showValueTemp6;
-signed long showValueTemp7;
-signed long showValueTemp8;
+// 配列は0から始まることに注意。
+#define T1T_DECIMAL_DIGIT 1 // 小数桁数
+signed long t1tValue[8];
+
+
+// 液面レベル
+// H/Lの二者択一。UECSSHOWSTRING で表示する。
+const char WATER_LVL[] PROGMEM = "液面レベル";
+const char WLVL_HIGH[] PROGMEM = "HIGH";
+const char WLVL_LOW[] PROGMEM  = "LOW";
+const char *StrWATER_LVL[2] = {
+  WLVL_HIGH,
+  WLVL_LOW
+};
+boolean waterLevel;          // I/Oから信号を受電するため
+signed long ShowWaterLevel;  // 表示のインデックスのため
+
+// 圧力センサ
+// H/Lの二者択一。UECSSHOWSTRING で表示する。
+const char PRESS_LVL[] PROGMEM  = "圧力";
+const char PRESS_HIGH[] PROGMEM = "HIGH";
+const char PRESS_LOW[] PROGMEM  = "LOW";
+const char *StrPRESS_LVL[2] = {
+  PRESS_HIGH,
+  PRESS_LOW
+};
+boolean pressLevel;          // I/Oから信号を受電するため
+signed long ShowPressLevel;  // 表示のインデックスのため
+
+
+// CO2センサ
+// センサの値は設定しないので UECSSHOWDATAで表示する。
+// K33LP
+#define CO2_DIGIT 0 // 小数桁数
+const char CO2NAME[] PROGMEM = "CO2濃度";
+const char CO2NOTE1[] PROGMEM = "K33LP";
+const char CO2NOTE2[] PROGMEM = "K33ICB";
+const char CO2NOTE3[] PROGMEM = "合わせて";
+const char CO2UNIT[] PROGMEM= "ppm";
+signed long co2detail;
+signed long co2bigger;
+signed long co2val;
+
+#define CO2TEMP_DIGIT  1
+#define CO2HUMI_DIGIT  1
+const char CO2TEMP_NAME[] PROGMEM = "CO2温度";
+const char CO2HUMI_NAME[] PROGMEM = "CO2湿度";
+const char CO2HUMI_UNIT[] PROGMEM = "%RH";
+singed long co2temp;
+signed long co2humi;
+
+
+
 
 //バルブの設定内容(強制OPEN/CLOSE,自動)
 //UECSSELECTDATA
-const char VLVNAME1[] PROGMEM= "バルブ1の設定";
-const char VLVNAME2[] PROGMEM= "バルブ2の設定";
-const char VLVNAME3[] PROGMEM= "バルブ3の設定";
-const char VLVNAME4[] PROGMEM= "バルブ4の設定";
-const char VLVNAME5[] PROGMEM= "バルブ5の設定";
-const char VLVNAME6[] PROGMEM= "バルブ6の設定";
-const char VLVNAME7[] PROGMEM= "バルブ7の設定";
-const char VLVNAME8[] PROGMEM= "バルブ8の設定";
+const char VLVNAME0[] PROGMEM= "バルブ1の設定";
+const char VLVNAME1[] PROGMEM= "バルブ2の設定";
+const char VLVNAME2[] PROGMEM= "バルブ3の設定";
+const char VLVNAME3[] PROGMEM= "バルブ4の設定";
+const char VLVNAME4[] PROGMEM= "バルブ5の設定";
+const char VLVNAME5[] PROGMEM= "バルブ6の設定";
+const char VLVNAME6[] PROGMEM= "バルブ7の設定";
+const char VLVNAME7[] PROGMEM= "バルブ8の設定";
+const char VLVNOTE0[] PROGMEM= "";
 const char VLVNOTE1[] PROGMEM= "";
 const char VLVNOTE2[] PROGMEM= "";
 const char VLVNOTE3[] PROGMEM= "";
@@ -89,37 +136,30 @@ const char VLVNOTE4[] PROGMEM= "";
 const char VLVNOTE5[] PROGMEM= "";
 const char VLVNOTE6[] PROGMEM= "";
 const char VLVNOTE7[] PROGMEM= "";
-const char VLVNOTE8[] PROGMEM= "";
-const char UECSOFF[] PROGMEM= "手動CLOSE";
-const char UECSON[] PROGMEM= "手動OPEN";
-const char UECSAUTO[] PROGMEM= "自動";
-const char *stringSELECT[3]={
-  UECSOFF,
-  UECSON,
-  UECSAUTO,
+const char VLV_CLOSE[] PROGMEM= "手動CLOSE";
+const char VLV_OPEN[] PROGMEM= "手動OPEN";
+const char VLV_AUTO[] PROGMEM= "自動";
+const char *StrVLV_SELECT[3]={
+  VLV_AUTO,
+  VLV_CLOSE,
+  VLV_OPEN,
 };
 
 //入力された選択肢の位置を受け取る変数
-//UECSOFFが0、UECSONで1、UECSAUTOで2になる
-signed long setONOFFAUTO1;
-signed long setONOFFAUTO2;
-signed long setONOFFAUTO3;
-signed long setONOFFAUTO4;
-signed long setONOFFAUTO5;
-signed long setONOFFAUTO6;
-signed long setONOFFAUTO7;
-signed long setONOFFAUTO8;
+// VLV_AUTOが0、VLV_CLOSEで1、VLV_OPENで2になる
+signed long set_VLV_SELECT[8];
 
 //●バルブ動作条件の入力
 //UECSINPUTDATA
-const char VCTNAME1[] PROGMEM= "V1作動温度";
-const char VCTNAME2[] PROGMEM= "V2作動温度";
-const char VCTNAME3[] PROGMEM= "V3作動温度";
-const char VCTNAME4[] PROGMEM= "V4作動温度";
-const char VCTNAME5[] PROGMEM= "V5作動温度";
-const char VCTNAME6[] PROGMEM= "V6作動温度";
-const char VCTNAME7[] PROGMEM= "V7作動温度";
-const char VCTNAME8[] PROGMEM= "V8作動温度";
+const char VCTNAME0[] PROGMEM= "V1作動温度";
+const char VCTNAME1[] PROGMEM= "V2作動温度";
+const char VCTNAME2[] PROGMEM= "V3作動温度";
+const char VCTNAME3[] PROGMEM= "V4作動温度";
+const char VCTNAME4[] PROGMEM= "V5作動温度";
+const char VCTNAME5[] PROGMEM= "V6作動温度";
+const char VCTNAME6[] PROGMEM= "V7作動温度";
+const char VCTNAME7[] PROGMEM= "V8作動温度";
+const char VCTNOTE0[] PROGMEM= "";
 const char VCTNOTE1[] PROGMEM= "";
 const char VCTNOTE2[] PROGMEM= "";
 const char VCTNOTE3[] PROGMEM= "";
@@ -127,18 +167,28 @@ const char VCTNOTE4[] PROGMEM= "";
 const char VCTNOTE5[] PROGMEM= "";
 const char VCTNOTE6[] PROGMEM= "";
 const char VCTNOTE7[] PROGMEM= "";
-const char VCTNOTE8[] PROGMEM= "";
 
 //バルブ動作温度を収める変数
 //小数桁数が1の場合、例えばWeb上で12.3が入力されると123が代入される
-signed long setONTempFromWeb1;
-signed long setONTempFromWeb2;
-signed long setONTempFromWeb3;
-signed long setONTempFromWeb4;
-signed long setONTempFromWeb5;
-signed long setONTempFromWeb6;
-signed long setONTempFromWeb7;
-signed long setONTempFromWeb8;
+signed long setVCTval[8];
+//signed long setONTempFromWeb2;
+
+// モーターポンプ、ブロア関係
+//
+const char MOTONAME0[] PROGMEM= "ブロアー";
+const char MOTONAME1[] PROGMEM= "ポンプ";
+const char MOTO_RUN[] PROGMEM  ="RUN";
+const char MOTO_STOP[] PROGMEM = "STOP";
+const char MOTO_AUTO[] PROGMEM = "AUTO";
+const char *StrMOTOSW[3] = {
+  MOTO_AUTO,
+  MOTO_RUN,
+  MOTO_STOP
+};
+
+signed log setMOTO_ON_OFF_AUTO[2];
+signed log statusMOTO_ON_OFF_AUTO[2];
+
 
 //●表示素材の定義(4)文字表示
 //UECSSHOWSTRING
@@ -150,14 +200,8 @@ const char *stringSHOW[2]={
   SHOWSTRING_OFF,
   SHOWSTRING_ON,
 };
-signed long showValueStatus1;
-signed long showValueStatus2;
-signed long showValueStatus3;
-signed long showValueStatus4;
-signed long showValueStatus5;
-signed long showValueStatus6;
-signed long showValueStatus7;
-signed long showValueStatus8;
+signed long showValueStatus[8];
+//signed long showValueStatus2;
 
 //●ダミー素材の定義
 //dummy value
@@ -300,6 +344,13 @@ void UserInit(){
 
 void OnWebFormRecieved() {
   U_ccmList[CCMID_OPETemp1].value=setONTempFromWeb1;
+  U_ccmList[CCMID_OPETemp2].value=setONTempFromWeb2;
+  U_ccmList[CCMID_OPETemp3].value=setONTempFromWeb3;
+  U_ccmList[CCMID_OPETemp4].value=setONTempFromWeb4;
+  U_ccmList[CCMID_OPETemp5].value=setONTempFromWeb5;
+  U_ccmList[CCMID_OPETemp6].value=setONTempFromWeb6;
+  U_ccmList[CCMID_OPETemp7].value=setONTempFromWeb7;
+  U_ccmList[CCMID_OPETemp8].value=setONTempFromWeb8;
   ChangeThermostat();
  
 }
@@ -333,6 +384,13 @@ void setup(){
 //---------------------------------------------------------
 void ChangeThermostat(){
   showValueTemp1 = U_ccmList[CCMID_TCTemp1].value;
+  showValueTemp2 = U_ccmList[CCMID_TCTemp2].value;
+  showValueTemp3 = U_ccmList[CCMID_TCTemp3].value;
+  showValueTemp4 = U_ccmList[CCMID_TCTemp4].value;
+  showValueTemp5 = U_ccmList[CCMID_TCTemp5].value;
+  showValueTemp6 = U_ccmList[CCMID_TCTemp6].value;
+  showValueTemp7 = U_ccmList[CCMID_TCTemp7].value;
+  showValueTemp8 = U_ccmList[CCMID_TCTemp8].value;
 
   switch(setONOFFAUTO1) {
   case 0:
@@ -344,6 +402,125 @@ void ChangeThermostat(){
   case 2:
     if (U_ccmList[CCMID_TCTemp1].validity) {
       if (U_ccmList[CCMID_TCTemp1].value<U_ccmList[CCMID_OPETemp1].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO2) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp2].validity) {
+      if (U_ccmList[CCMID_TCTemp2].value<U_ccmList[CCMID_OPETemp2].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO3) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp3].validity) {
+      if (U_ccmList[CCMID_TCTemp3].value<U_ccmList[CCMID_OPETemp3].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO4) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp4].validity) {
+      if (U_ccmList[CCMID_TCTemp4].value<U_ccmList[CCMID_OPETemp4].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO5) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp5].validity) {
+      if (U_ccmList[CCMID_TCTemp5].value<U_ccmList[CCMID_OPETemp5].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO6) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp6].validity) {
+      if (U_ccmList[CCMID_TCTemp6].value<U_ccmList[CCMID_OPETemp6].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO7) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp7].validity) {
+      if (U_ccmList[CCMID_TCTemp7].value<U_ccmList[CCMID_OPETemp7].value) {
+	U_ccmList[CCMID_cnd].value=1;  //Auto ON
+	break;
+      }
+    }
+    U_ccmList[CCMID_cnd].value=0;  //Auto OFF
+    break;
+  }
+  switch(setONOFFAUTO8) {
+  case 0:
+    U_ccmList[CCMID_cnd].value=0;  //Manual OFF
+    break;
+  case 1:
+    U_ccmList[CCMID_cnd].value=1;  //Manual ON
+    break;
+  case 2:
+    if (U_ccmList[CCMID_TCTemp8].validity) {
+      if (U_ccmList[CCMID_TCTemp8].value<U_ccmList[CCMID_OPETemp8].value) {
 	U_ccmList[CCMID_cnd].value=1;  //Auto ON
 	break;
       }
